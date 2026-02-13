9d301b4b72d8058d67adeb625455f8045c72182f
winning godspower <winninggodspower@gmail.com>
2026-02-13 19:06:14 +0100

    feat: edit footer to match figma

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,43 +1,40 @@
 'use client'
 
-import { Heart } from 'lucide-react'
 
 export default function Footer() {
   return (
-    <footer className="bg-[#403F3F] text-white py-12 md:py-20">
-      <div className="max-w-6xl mx-auto px-6">
+    <footer className="bg-[#38332E] text-white pt-20 pb-14 md:pt-10 md:pb-16">
+      <div className="max-w-6xl mx-auto">
         {/* Main Content */}
-        <div className="text-center space-y-6">
+        <div className="text-center">
           {/* Names with Heart */}
-          <div className="flex items-center justify-center gap-4 md:gap-6">
-            <span className="text-4xl md:text-6xl font-light font-inspo">P</span>
-            <Heart className="w-6 h-6 md:w-8 md:h-8 fill-primary text-primary" />
-            <span className="text-4xl md:text-6xl font-light font-inspo">V</span>
+          <div className="flex items-center justify-center gap-4 md:gap-8 font-junge">
+            <span className="text-4xl md:text-6xl">P</span>
+             <img className='h-11' src={'/solar-heart-shine.svg'} />
+            <span className="text-4xl md:text-6xl">V</span>
           </div>
 
           {/* Date and Location */}
-          <p className="text-sm md:text-base font-light tracking-wide">
-            June 20th, 2026, Lagos, Nigeria
+          <p className="text-sm md:text-base">
+            June 20th, 2026. Lagos, Nigeria
           </p>
 
-          {/* Divider */}
-          <div className="h-px bg-white/20 my-6 md:my-8 mx-auto max-w-md"></div>
 
           {/* Contact Section */}
-          <div className="space-y-2">
-            <p className="text-xs md:text-sm tracking-widest uppercase font-light">
+          <div className="space-y-1 mt-9 md:mt-10">
+            <p className="text-sm md:text-base uppercase font-medium">
               Questions? Contact us at
             </p>
-            <p className="text-sm md:text-base font-light">
+            <p className="text-sm md:text-base">
               P&V2026.com
             </p>
           </div>
 
           {/* Divider */}
-          <div className="h-px bg-white/20 my-6 md:my-8 mx-auto max-w-md"></div>
+          <div className="h-px bg-white/20 my-6 md:my-8 mx-auto md:max-w-7xl"></div>
 
           {/* Footer Message */}
-          <p className="text-xs md:text-sm font-light tracking-wide">
+          <p className="text-xs md:text-sm tracking-wide px-2">
             Made with love for praise and Victor's special day
           </p>
         </div>
